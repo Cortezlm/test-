33791d1355a79c184ffd9eddde7a0f2992377ad1
Cortez  Martin <cortez2726@CortezMartin.attlocal.net>
2024-08-19 19:02:01 -0500

    remember

--- bash.sh
+++ bash.sh
@@ -1,4 +1,10 @@
-echo "hello"
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author: cortez martin
+# Date of latest revision: 09/19/24
+# Purpose: Print a string to the terminal
+
 echo "class"
 echo "this is cool"
 echo "awsome"
