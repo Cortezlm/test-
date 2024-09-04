530cb8d0f56db3c8e22e59d21a9da76e55b5e578
Cortez  Martin <cortez2726@CortezMartin.attlocal.net>
2024-09-03 19:49:20 -0500

    bash4.sh

--- bash.sh
+++ bash.sh
@@ -11,3 +11,39 @@ echo "awsome"
 
 name=Cortez 
 echo "welcome to the class $name"
+
+#!/bin/bash
+# Condtinal Statment 
+# This ops challenges is about conditinal stamtment of if else statments and how they work
+# We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
+
+
+function hello(){
+    ipconfig
+    echo "How are you doing"
+    Ls 
+}
+function add(){
+    num1=7
+    num2=5
+    echo $((num1 - num2))
+}
+add
+
+
+#!/bin/bash
+# Install whois on your Ubuntu
+
+# Add to your bash script a sixth option that calls a function to:
+
+# Take a user input string. Presumably the string is a domain name such as Google.com.
+read -bash "enter a domain name" ./domain
+echo $domain
+# Run whois against a user input string.
+# Run dig against the user input string.
+# Run host against the user input string.
+# Run nslookup against the user input string.
+# Output the results to a single .txt file and open it with your favorite text editor.
+
+# For this challenge you must use at least one variable and one function.
+
---
+++ bash2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Install whois on your Ubuntu
+
+# Add to your bash script a sixth option that calls a function to:
+
+# Take a user input string. Presumably the string is a domain name such as Google.com.
+read -p "enter a domain name" domain
+echo $domain
+# Run whois against a user input string.
+function dowhois() {
+    {
+     whois $domain
+    } >> "output.txt"
+} 
+dowhois
+
+# Run dig against the user input string.
+# Run host against the user input string.
+# Run nslookup against the user input string.
+# Output the results to a single .txt file and open it with your favorite text editor.
+
+# For this challenge you must use at least one variable and one function.
+
+function dig() {
+    {
+     dig $domain
+    } >> "output.txt"
+} 
+dig
+
